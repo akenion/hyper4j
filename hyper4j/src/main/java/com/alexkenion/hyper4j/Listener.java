5f15e14fa31a98730ea6a06c667f7c79ffa64756
Alex Kenion <alex.kenion@gmail.com>
2021-01-21 23:58:30 -0500

    Removed extraneous files

--- hyper4j/src/main/java/com/alexkenion/hyper4j/Listener.java
+++
@@ -1,7 +0,0 @@
-package com.alexkenion.hyper4j;
-
-public interface Listener {
-	
-	public void listen();
-
-}
--- hyper4j/src/main/java/com/alexkenion/hyper4j/Protocol.java
+++
@@ -1,7 +0,0 @@
-package com.alexkenion.hyper4j;
-
-public interface Protocol {
-	
-	public boolean test();
-
-}
